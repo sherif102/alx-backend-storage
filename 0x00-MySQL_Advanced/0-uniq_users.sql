464d8cbe54c9845e38b416ec5f57dd62fb2cbcb0
hyperbayse <hyperbayse@gmail.com>
2025-05-26 00:28:17 +0100

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,3 @@
--- Write a SQL script that creates a table users
-
-CREATE TABLE IF NOT EXISTS users (
-    id int NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
-);
+-- we are all unique!
+-- creates table 'users'
+CREATE TABLE IF NOT EXISTS users (`id` INT  PRIMARY KEY NOT NULL AUTO_INCREMENT, `email` VARCHAR(255) NOT NULL UNIQUE, `name` VARCHAR(255))
