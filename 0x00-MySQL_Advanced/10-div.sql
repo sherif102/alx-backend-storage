fa93cec55db3dabe9c38d38e84d1c687b26dc633
hyperbayse <hyperbayse@gmail.com>
2025-05-30 02:19:34 +0100

    10-div.sql

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,12 @@
+-- safe divide
+-- create a function 'SafeDiv'
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER //
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    IF b = 0 THEN RETURN 0;
+    END IF;
+    RETURN a / b;
+END; //
+DELIMITER ;
